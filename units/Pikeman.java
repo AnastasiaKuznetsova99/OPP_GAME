dfae34e7a6da556cf6c09208ffa68ff0d884b048
Anastasia Kuznetsova <asiakisa11@gmail.com>
2023-07-20 20:27:26 +0500

    add HW4, HW5,HW6

---
+++ units/Pikeman.java
@@ -0,0 +1,9 @@
+package units;
+
+public class Pikeman extends Infantry {
+    public Pikeman(String name, int x, int y, int nT) {
+        super(45, name, new int[]{2,3}, 5, 2, x, y, nT, 5);
+    }
+
+
+}
